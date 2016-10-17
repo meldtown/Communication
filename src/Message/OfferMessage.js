433c10399d3fc97397656778a38b917c8d6c3fa3
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2016-10-17 21:57:29 +0300

    offer message vacancy prop

--- src/Message/OfferMessage.js
+++ src/Message/OfferMessage.js
@@ -1,4 +1,10 @@
+import * as ko from 'knockout'
 import AbstractMessage from './AbstractMessage'
 
 export default class OfferMessage extends AbstractMessage {
+	constructor(data = {}) {
+		super(data)
+		let {vacancy} = data
+		this.vacancy = ko.observable(vacancy)
+	}
 }
--- src/Message/OfferMessage.spec.js
+++ src/Message/OfferMessage.spec.js
@@ -1,14 +1,20 @@
+import * as ko from 'knockout'
 import assert from 'assert'
 import OfferMessage from './OfferMessage'
 import AbstractMessage from './AbstractMessage'
 
 describe('OfferMessage', () => {
-	it('should be instantiable', () => {
-		let model = new OfferMessage()
-		assert.equal(model instanceof OfferMessage, true)
-		assert.equal(model instanceof AbstractMessage, true)
-
+	let model
+	beforeEach(() => {
+		model = new OfferMessage()
 	})
 
+	it('should be instantiable', () => {
+		assert.equal(model instanceof OfferMessage, true)
+		assert.equal(model instanceof AbstractMessage, true)
+	})
 
+	it('should have vacancy', () => {
+		assert.equal(ko.isObservable(model.vacancy), true)
+	})
 })
