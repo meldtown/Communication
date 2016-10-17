2502cfa657c770d3a48c9d26a687dbf6d748f258
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2016-10-17 17:13:48 +0300

    invite template fields

--- src/Templates/Form/InviteTemplateForm.js
+++ src/Templates/Form/InviteTemplateForm.js
@@ -1,8 +1,11 @@
+import * as ko from 'knockout'
 import AbstractTemplateForm from './AbstractTemplateForm'
 
 export default class InviteTemplateForm extends AbstractTemplateForm {
 	constructor() {
 		super()
+		this.time = ko.observable()
+		this.address = ko.observable()
 	}
 }
 
--- src/Templates/Form/InviteTemplateForm.spec.js
+++ src/Templates/Form/InviteTemplateForm.spec.js
@@ -1,9 +1,23 @@
+import * as ko from 'knockout'
 import assert from 'assert'
 import InviteTemplateForm from './InviteTemplateForm'
 
 describe('InviteTemplateForm', () => {
+	let model
+
+	beforeEach(() => {
+		model = new InviteTemplateForm()
+	})
+
 	it('should be instantiable', () => {
-		let model = new InviteTemplateForm()
 		assert.equal(model instanceof InviteTemplateForm, true)
+	})
+
+	it('should have time', () => {
+		assert.equal(ko.isObservable(model.time), true)
+	})
+
+	it('should have address', () => {
+		assert.equal(ko.isObservable(model.address), true)
 	})
 })
