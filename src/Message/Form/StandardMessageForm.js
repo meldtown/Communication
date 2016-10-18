8e8f3c823f9d5b341524c1c6e942a9591b9a4aa0
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2016-10-18 11:26:22 +0300

    fire NEW_MESSAGE event on successful save

--- src/Message/Form/StandardMessageForm.js
+++ src/Message/Form/StandardMessageForm.js
@@ -1,9 +1,33 @@
+import * as actions from '../../actions'
+import * as types from '../types'
+import $ from 'jquery'
 import AbstractMessageForm from './AbstractMessageForm'
+import MessageFactory from '../MessageFactory'
 
 export default class StandardMessageForm extends AbstractMessageForm {
 	save() {
 		if (!this.conversationId()) {
 			throw new Error('conversationId is required')
 		}
+
+		return $.post(`${api}/messages`, {
+			type: types.STANDARD,
+			conversationId: this.conversationId(),
+			text: this.text()
+		}).then(data => {
+			if (this.reset) {
+				this.reset()
+			}
+
+			let message = MessageFactory.create(data)
+
+			this.dispatcher.notifySubscribers(message, actions.NEW_MESSAGE)
+
+			return message
+		})
+	}
+
+	reset() {
+		this.text('')
 	}
 }
