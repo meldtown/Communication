fbcd5eee0e7140f5039e24fdbdad24975ced1c00
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2016-10-18 13:49:26 +0300

    decline message form save initial

--- src/Message/Form/DeclineMessageForm.js
+++ src/Message/Form/DeclineMessageForm.js
@@ -1,4 +1,33 @@
 import AbstractMessageForm from './AbstractMessageForm'
+import MessageFactory from '../MessageFactory'
+import $ from 'jquery'
+import * as types from '../types'
+import * as actions from '../../actions'
 
 export default class DeclineMessageForm extends AbstractMessageForm {
+	save() {
+		if (!this.conversationId()) {
+			throw new Error('conversationId is required')
+		}
+
+		return $.post(`${api}/messages`, {
+			type: types.DECLINE,
+			conversationId: this.conversationId(),
+			text: this.text()
+		}).then(data => {
+			if (this.reset) {
+				this.reset()
+			}
+
+			let message = MessageFactory.create(data)
+
+			this.dispatcher.notifySubscribers(message, actions.NEW_MESSAGE)
+
+			return message
+		})
+	}
+
+	reset() {
+		this.text('')
+	}
 }
--- src/Message/Form/DeclineMessageForm.spec.js
+++ src/Message/Form/DeclineMessageForm.spec.js
@@ -1,10 +1,103 @@
 import * as ko from 'knockout'
 import assert from 'assert'
 import DeclineMessageForm from './DeclineMessageForm'
+import DeclineMessage from '../DeclineMessage'
+import * as types from '../types'
+import * as actions from '../../actions'
+import $ from 'jquery'
+import jQueryMockAjax from 'jquery-mockjax'
+
+const api = 'http://sample.com'
+const mockjax = jQueryMockAjax($, window)
+$.mockjaxSettings.logging = 0
 
 describe('DeclineMessageForm', () => {
+	let model
+	let dispatcher
+
+	before(() => {
+		global.api = api
+	})
+
+	beforeEach(() => {
+		dispatcher = new ko.subscribable()
+		model = new DeclineMessageForm(dispatcher)
+	})
+
+	afterEach(() => mockjax.clear())
+
 	it('should be instantiable', () => {
-		let model = new DeclineMessageForm(new ko.subscribable())
 		assert.equal(model instanceof DeclineMessageForm, true)
+	})
+
+	it('should have save method', () => {
+		assert.equal(typeof model.save, 'function')
+	})
+
+	it('should not try save message without conversationId', () => {
+		assert.throws(() => model.save(), Error)
+	})
+
+	it(`should call api on save`, () => {
+		let conversationId = 1
+		let text = 'Hello World'
+
+		model.conversationId(conversationId)
+		model.text(text)
+
+		mockjax({
+			type: 'post',
+			url: `${api}/messages`,
+			responseText: {conversationId, text, id: 1, type: types.DECLINE, date: (new Date()).toISOString()}
+		})
+
+		return model.save().then(message => {
+			assert.equal(message instanceof DeclineMessage, true)
+			assert.equal(message.text(), text)
+			assert.equal(message.conversationId(), conversationId)
+			assert.equal(message.id(), 1)
+		})
+	})
+
+	it('should reset form on successful save', () => {
+		let conversationId = 1
+		let text = 'Hello World'
+
+		model.conversationId(conversationId)
+		model.text(text)
+
+		mockjax({
+			type: 'post',
+			url: `${api}/messages`,
+			responseText: {conversationId, text, id: 1, type: types.DECLINE, date: (new Date()).toISOString()}
+		})
+
+		return model.save().then(() => {
+			assert.equal(model.text(), '')
+		})
+	})
+
+	it(`should fire ${actions.NEW_MESSAGE} on successful save`, () => {
+		let counter = 0
+
+		dispatcher.subscribe(() => {
+			counter = counter + 1
+		}, null, actions.NEW_MESSAGE)
+
+		let conversationId = 1
+		let text = 'Hello World'
+
+		model.conversationId(conversationId)
+		model.text(text)
+
+		mockjax({
+			type: 'post',
+			url: `${api}/messages`,
+			responseText: {conversationId, text, id: 1, type: types.DECLINE, date: (new Date()).toISOString()}
+		})
+
+		return model.save().then(() => {
+			assert.equal(counter, 1)
+		})
 	})
 })
