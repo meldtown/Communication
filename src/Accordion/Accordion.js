48df315f57329a0e513588b3c12ff7dc354c62e7
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2016-10-19 10:38:11 +0300

    accordion dispatcher

--- src/Accordion/Accordion.js
+++ src/Accordion/Accordion.js
@@ -1,5 +1,11 @@
-export default class Accordion {
-	constructor() {
+import * as ko from 'knockout'
 
+export default class Accordion {
+	constructor(dispatcher) {
+		if (!ko.isSubscribable(dispatcher)) {
+			throw new Error('ko.subscribable is required')
+		}
+
+		this.dispatcher = dispatcher
 	}
 }
