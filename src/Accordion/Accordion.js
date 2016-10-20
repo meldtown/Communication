7de3d62e47b8ec06d7f82f45e427b0ccc7537573
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2016-10-20 13:10:02 +0300

    accordion conversationId and messages

--- src/Accordion/Accordion.js
+++ src/Accordion/Accordion.js
@@ -1,11 +1,26 @@
 import * as ko from 'knockout'
+import MessageList from '../Message/MessageList'
 
 export default class Accordion {
-	constructor(dispatcher) {
+	constructor(dispatcher, conversationId) {
 		if (!ko.isSubscribable(dispatcher)) {
 			throw new Error('ko.subscribable is required')
 		}
 
+		if (!conversationId) {
+			throw new Error('conversationId is required')
+		}
+
 		this.dispatcher = dispatcher
+		this.conversationId = ko.observable(conversationId)
+		this.messages = new MessageList(dispatcher)
+
+		this.conversationId.subscribe(conversationId => {
+			this.messages.conversationId(conversationId)
+		})
+	}
+
+	fetch() {
+
 	}
 }
