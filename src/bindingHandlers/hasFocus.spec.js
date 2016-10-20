0c833eb8797c8690502238c63419db05f6591a75
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2016-10-20 18:16:57 +0300

    hasFocus more tests for full coverage

--- src/bindingHandlers/hasFocus.spec.js
+++ src/bindingHandlers/hasFocus.spec.js
@@ -1,7 +1,7 @@
 import assert from 'assert'
 import * as ko from 'knockout'
 import $ from 'jquery'
-import './hasFocus'
+import hasFocus from './hasFocus'
 
 describe('hasFocus', () => {
 	it('can work with jquery', () => {
@@ -21,6 +21,37 @@ describe('hasFocus', () => {
 
 		document.querySelector('#hasFocus input').focus()
 		assert.equal(model.hasFocus(), true)
+	})
+
+	it('should set focus and blur handlers', () => {
+		let observable = ko.observable(false)
+		let accessor = () => observable
+		let input = document.createElement('INPUT')
+		hasFocus.init(input, accessor)
+		assert.equal(observable(), false)
+		input.focus()
+		assert.equal(observable(), true)
+		input.blur()
+		assert.equal(observable(), false)
+	})
+
+	it('should call focus or blur on update', () => {
+		let observable = ko.observable(false)
+		let accessor = () => observable
+		let input = document.createElement('INPUT')
+		let focusCounter = 0
+		let blurCounter = 0
+		input.focus = () => focusCounter += 1
+		input.blur = () => blurCounter += 1
+
+		hasFocus.update(input, accessor)
+		assert.equal(blurCounter, 1)
+
+		observable(true)
+		hasFocus.update(input, accessor)
+		assert.equal(focusCounter, 1)
+		assert.equal(blurCounter, 1)
+
 	})
 })
 
