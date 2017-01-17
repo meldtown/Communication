566d77dc364b63ad0503eb77e6dae4e0e252d38f
RABOTA\VitaliyM <meldtown@gmail.com>
2017-01-17 17:31:25 +0200

    Fix languages displaying at templates

--- src/Templates/AbstractTemplate.js
+++ src/Templates/AbstractTemplate.js
@@ -1,9 +1,18 @@
 import * as ko from 'knockout'
+import * as languages from '../constants'
 
 export default class AbstractTemplate {
 	constructor(dispatcher, {id, name, text, language} = {}) {
 		if (!ko.isSubscribable(dispatcher)) {
 			throw new Error('ko.subscribable is required')
+		}
+		switch (language) {
+			case 1: language = languages.RU
+				break
+			case 2: language = languages.UA
+				break
+			case 3: language = languages.EN
+				break
 		}
 
 		this.id = ko.observable(id)
