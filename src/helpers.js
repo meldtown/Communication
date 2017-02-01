a5a5ecef68a93b98c05a654abe3413a5cfdb3446
Vlad Feninets <v.feninets@gmail.com>
2017-02-01 17:03:25 +0200

    formatting for agoTime

--- src/helpers.js
+++ src/helpers.js
@@ -19,3 +19,22 @@ export const formattedDate = date => date && !isNaN(Date.parse(date)) && moment(
 export const formattedTime = date => date && !isNaN(Date.parse(date)) && moment(date).isValid() ? moment(date).format('HH:mm') : null
 export const isoDateTime = date => date && !isNaN(Date.parse(date)) && moment(date).isValid() ? moment(date).format() : null
 export const inputFormattedDate = date => date && !isNaN(Date.parse(date)) && moment(date).isValid() ? moment(date).format('YYYY-MM-DD') : null
+
+export const formattedAgo = date => {
+	if( date && !isNaN(Date.parse(date)) && moment(date).isValid() ) {
+		const momented = moment(date)
+
+		if( momented.isSame(moment(), 'day') ) {
+			return momented.format('HH:mm')
+		}
+		if( momented.isBetween(moment().subtract(7,'d'), moment(), 'day') ) {
+			return momented.format('ddd')
+		}
+		if( momented.isSame(moment(), 'year') ) {
+			return momented.format('DD MMM')
+		}
+
+		return momented.format('DD/MM/YYYY')
+	}
+	return null
+}
